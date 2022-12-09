909749039ea6ec997bb5f1d40affc0fda738abcd
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-09 17:20:06 +0900

    feat: useStaticQuery 처리

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
 
 interface ISeoProps {
   title: string;
 }
 const Seo = (props: ISeoProps) => {
+  const data = useStaticQuery<Queries.SeoDataQuery>(graphql`
+    query SeoData {
+      site{
+        siteMetadata {
+          title 
+        }
+      }
+    }
+  `);
+
   return (
-    <title>{props.title} | DevStickers!</title>
+    <title>{props.title} | {data.site?.siteMetadata?.title}</title>
   );
 };
 
