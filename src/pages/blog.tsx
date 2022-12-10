eb59fd2212f6c222d816e6804a5925c1054842c2
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-11 00:00:01 +0900

    feat: expot const query 생성 및 props 설정

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,14 +1,28 @@
 import React from "react";
+import { graphql, PageProps } from "gatsby";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 
-const Blog = () => {
+// eslint-disable-next-line no-undef
+const Blog = (props: PageProps<Queries.BlogTitlesQuery>) => {
   return (
     <Layout title="blog">
-      <div />
+      <ul>
+        {props.data.allFile.nodes.map((item, index) => <li key={index}>{item.name}</li>)}
+      </ul>
     </Layout>
   );
 };
+
+export const query = graphql`
+  query BlogTitles {
+      allFile {
+          nodes {
+              name
+          }
+      }
+  }
+`;
 
 export const Head = () => <Seo title="Blog" />;
 
