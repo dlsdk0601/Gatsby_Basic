463a986d77f361ba04d791eb1da2db1f0fcdcc63
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-13 23:53:51 +0900

    refactor: mdx 플러그인에 추가

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,12 +11,13 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: "gatsby-source-filesystem",
-    options: {
-      path: `${__dirname}/blog-posts`
-    }
-  }],
+  plugins: [
+    "gatsby-plugin-mdx", {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/blog-posts`,
+      },
+    }],
 };
 
 export default config;
