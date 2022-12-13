80cbc12c009e5472f62ee2bca182f0ea6576c739
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-14 02:41:01 +0900

    feat: staticImage 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import type { PageProps } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 
@@ -7,6 +8,7 @@ const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout title="Welcome to DevStickers ">
       <div />
+      <StaticImage src="https://images.unsplash.com/photo-1625768376503-68d2495d78c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80" alt="sample" />
     </Layout> 
   );
 };
