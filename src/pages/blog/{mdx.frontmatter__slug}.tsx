350991b556328af9a7f2aab25ee6e9672216bee2
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-14 02:30:40 +0900

    refactor: query,head 추가

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,12 +1,37 @@
 import React from "react";
+import { graphql } from "gatsby";
 import Layout from "../../components/Layout";
+import Seo from "../../components/Seo";
 
-const BlogPost = () => {
+interface IBlogPostProps {
+  data: Queries.PostDetailQuery;
+  children?: any
+}
+
+const BlogPost = ({ data, children }: IBlogPostProps) => {
   return (
     <Layout title="Blog post">
-      <div />
+      <div>{children}</div>
     </Layout>
   );
 };
 
+export const query = graphql`
+    query PostDetail($frontmatter__slug: String) {
+        mdx(frontmatter: {slug: {eq: 
+            $frontmatter__slug
+        }}){
+            body
+            frontmatter {
+                author,
+                title,
+                category,
+                date,
+                slug
+            }
+        }
+    }
+`;
+
+export const Head = ({ data }: IBlogPostProps) => <Seo title={data.mdx?.frontmatter?.title ?? ""} />;
 export default BlogPost;
